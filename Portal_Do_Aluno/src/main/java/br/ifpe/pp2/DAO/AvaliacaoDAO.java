6002d5739c179dd91d402c91aed59ff51b1bcf97
matias1817 <matias21.oliveirape@gmail.com>
2021-07-23 17:20:03 -0300

    melhoras nos filtros

--- Portal_Do_Aluno/src/main/java/br/ifpe/pp2/DAO/AvaliacaoDAO.java
+++ Portal_Do_Aluno/src/main/java/br/ifpe/pp2/DAO/AvaliacaoDAO.java
@@ -13,22 +13,22 @@ import br.ifpe.pp2.entities.Periodo;
 public interface AvaliacaoDAO extends JpaRepository<Avaliacao, Integer> {
 	public Avaliacao findByalunosAndMateriasAndPeriodo(Alunos aluno, Materias materias, Periodo periodo);
 	
-	@Query(value = "SELECT av FROM Avaliacao av, Alunos al, Materias ma, Periodo pe WHERE av.alunos.id = al.id and av.materias.id = ma.id and av.periodo.id = pe.id and  av.alunos.nome like :nomeCliente and av.materias.id = :materia and av.periodo.id = :periodo")
+	@Query(value = "SELECT av FROM Avaliacao av, Alunos al, Materias ma, Periodo pe WHERE av.alunos.id = al.id and av.materias.id = ma.id and av.periodo.id = pe.id and  av.alunos.nome like '%'||:nomeCliente||'%' and av.materias.id = :materia and av.periodo.id = :periodo")
     List<Avaliacao> filtrarAvaliacao(String nomeCliente,Integer materia, Integer periodo);
 	
 	@Query(value = "SELECT av FROM Avaliacao av, Materias ma, Periodo pe WHERE av.materias.id = ma.id and av.periodo.id = pe.id and  av.materias.id = :materia and av.periodo.id = :periodo")
 	List<Avaliacao> filtrarAvaliacaoMateriaPeriodo(Integer materia, Integer periodo);
 	
-	@Query(value = "SELECT av FROM Avaliacao av, Alunos al, Materias ma WHERE av.alunos.id = al.id and av.materias.id = ma.id and  av.alunos.nome like :nomeCliente and av.materias.id = :materia")
+	@Query(value = "SELECT av FROM Avaliacao av, Alunos al, Materias ma WHERE av.alunos.id = al.id and av.materias.id = ma.id and  av.alunos.nome like '%'||:nomeCliente||'%' and av.materias.id = :materia")
 	List<Avaliacao> filtrarAvaliacaoNomeMateria(String nomeCliente,Integer materia);
 	
-	@Query(value = "SELECT av FROM Avaliacao av, Alunos al, Periodo pe WHERE av.alunos.id = al.id and av.periodo.id = pe.id and  av.alunos.nome like :nomeCliente and av.periodo.id = :periodo")
+	@Query(value = "SELECT av FROM Avaliacao av, Alunos al, Periodo pe WHERE av.alunos.id = al.id and av.periodo.id = pe.id and  av.alunos.nome like '%'||:nomeCliente||'%' and av.periodo.id = :periodo")
 	List<Avaliacao> filtrarAvaliacaoNomePeriodo(String nomeCliente, Integer periodo);
 	
 	@Query(value = "SELECT av FROM Avaliacao av,Periodo pe WHERE av.periodo.id = pe.id and av.periodo.id = :periodo")
 	List<Avaliacao> filtrarAvaliacaoPeriodo(Integer periodo);
 	
-	@Query(value = "SELECT av FROM Avaliacao av, Alunos al WHERE av.alunos.id = al.id and av.alunos.nome like :nomeCliente")
+	@Query(value = "SELECT av FROM Avaliacao av, Alunos al WHERE av.alunos.id = al.id and av.alunos.nome like '%'||:nomeCliente||'%'")
 	List<Avaliacao> filtrarAvaliacaoNome(String nomeCliente);
 	
 	@Query(value = "SELECT av FROM Avaliacao av, Materias ma WHERE av.materias.id = ma.id and av.materias.id = :materia")
--- Portal_Do_Aluno/src/main/java/br/ifpe/pp2/DAO/FrequenciaDAO.java
+++ Portal_Do_Aluno/src/main/java/br/ifpe/pp2/DAO/FrequenciaDAO.java
@@ -13,22 +13,22 @@ import br.ifpe.pp2.entities.Periodo;
 public interface FrequenciaDAO extends JpaRepository<Frequencia, Integer>{
 	public Frequencia findByalunosAndMateriasAndPeriodo(Alunos alunos, Materias materias, Periodo periodo);
 	
-	@Query(value = "SELECT fr FROM Frequencia fr, Alunos al, Materias ma, Periodo pe WHERE fr.alunos.id = al.id and fr.materias.id = ma.id and fr.periodo.id = pe.id and  fr.alunos.nome like :nomeCliente and fr.materias.id = :materia and fr.periodo.id = :periodo")
+	@Query(value = "SELECT fr FROM Frequencia fr, Alunos al, Materias ma, Periodo pe WHERE fr.alunos.id = al.id and fr.materias.id = ma.id and fr.periodo.id = pe.id and  fr.alunos.nome like '%'||:nomeCliente||'%' and fr.materias.id = :materia and fr.periodo.id = :periodo")
     List<Frequencia> filtrarFrequencia(String nomeCliente,Integer materia, Integer periodo);
 	
 	@Query(value = "SELECT fr FROM Frequencia fr, Materias ma, Periodo pe WHERE fr.materias.id = ma.id and fr.periodo.id = pe.id and  fr.materias.id = :materia and fr.periodo.id = :periodo")
 	List<Frequencia> filtrarFrequenciaMateriaPeriodo(Integer materia, Integer periodo);
 	
-	@Query(value = "SELECT fr FROM Frequencia fr, Alunos al, Materias ma WHERE fr.alunos.id = al.id and fr.materias.id = ma.id and  fr.alunos.nome like :nomeCliente and fr.materias.id = :materia")
+	@Query(value = "SELECT fr FROM Frequencia fr, Alunos al, Materias ma WHERE fr.alunos.id = al.id and fr.materias.id = ma.id and  fr.alunos.nome like '%'||:nomeCliente||'%' and fr.materias.id = :materia")
 	List<Frequencia> filtrarFrequenciaNomeMateria(String nomeCliente,Integer materia);
 	
-	@Query(value = "SELECT fr FROM Frequencia fr, Alunos al, Periodo pe WHERE fr.alunos.id = al.id and fr.periodo.id = pe.id and  fr.alunos.nome like :nomeCliente and fr.periodo.id = :periodo")
+	@Query(value = "SELECT fr FROM Frequencia fr, Alunos al, Periodo pe WHERE fr.alunos.id = al.id and fr.periodo.id = pe.id and  fr.alunos.nome like '%'||:nomeCliente||'%' and fr.periodo.id = :periodo")
 	List<Frequencia> filtrarFrequenciaNomePeriodo(String nomeCliente, Integer periodo);
 	
 	@Query(value = "SELECT fr FROM Frequencia fr,Periodo pe WHERE fr.periodo.id = pe.id and fr.periodo.id = :periodo")
 	List<Frequencia> filtrarFrequenciaPeriodo(Integer periodo);
 	
-	@Query(value = "SELECT fr FROM Frequencia fr, Alunos al WHERE fr.alunos.id = al.id and fr.alunos.nome like :nomeCliente")
+	@Query(value = "SELECT fr FROM Frequencia fr, Alunos al WHERE fr.alunos.id = al.id and fr.alunos.nome like '%'||:nomeCliente||'%'")
 	List<Frequencia> filtrarFrequenciaNome(String nomeCliente);
 	
 	@Query(value = "SELECT fr FROM Frequencia fr, Materias ma WHERE fr.materias.id = ma.id and fr.materias.id = :materia")
